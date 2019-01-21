b7281a124d1eb3016dfd523b57b03ee13b4f4b00
dani <dani.zaghian@gmail.com>
2019-01-21 11:46:14 -0800

    adds destroy action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -36,4 +36,10 @@ class Api::RecipesController < ApplicationController
 		render 'show.json.jbuilder'
 	end
 
+	def destroy
+		@recipe = Recipe.find(params[:id])
+		@recipe.destroy
+		render json: {message: "Recipe successfully destroyed."}
+	end
+
 end
