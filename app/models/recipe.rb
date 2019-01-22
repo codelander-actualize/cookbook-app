e318ede2499f0c22504f7849a63fd7a538c40fe6
dani <dani.zaghian@gmail.com>
2019-01-22 10:11:27 -0800

    adds model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,25 @@
 class Recipe < ApplicationRecord
+
+	def ingredients_array
+		ingredients.split(", ")
+	end
+
+	def directions_array
+		directions.split(", ")
+	end
+
+	def friendly_created_at
+		created_at.strftime("%A, %b %d")
+	end
+
+	def friendly_prep_time
+		result = ""
+		hours = prep_time/60
+		minutes = prep_time % 60
+		result += "#{hours} hours " if hours > 0
+		result += "#{minutes} minutes" if minutes > 0
+		result
+	end
+
 end
+
--- app/views/api/recipes/show.json.jbuilder
+++ app/views/api/recipes/show.json.jbuilder
@@ -4,3 +4,12 @@ json.chef @recipe.chef
 json.ingredients @recipe.ingredients
 json.directions @recipe.directions
 json.prep_time @recipe.prep_time
+json.image_url @recipe.image_url
+json.created_at @recipe.created_at
+
+json.formatted do
+	json.created_at @recipe.friendly_created_at
+	json.prep_time @recipe.friendly_prep_time
+	json.ingredients @recipe.ingredients_array
+	json.directions @recipe.directions_array
+end
