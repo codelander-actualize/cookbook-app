f41e3f791a4b1ac8ba8c5a6caa8449f377a48edb
dani <dani.zaghian@gmail.com>
2019-02-05 12:32:01 -0800

    adds implicit renders taken out

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,16 +2,14 @@ class RecipesController < ApplicationController
 
 	def index
 		@recipes = Recipe.all
-		render 'index.html.erb'
 	end
 
 	def new
-		render 'new.html.erb'
+		#IMPLICIT RENDERING OF HTML ONLY
 	end
 
 	def show
 		@recipe = Recipe.find(params[:id])
-		render 'show.html.erb'
 	end
 
 end
