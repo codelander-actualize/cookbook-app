44a4c4c051fff5f28b0632206034d8c57ef2fc03
dani <dani.zaghian@gmail.com>
2019-02-13 13:06:22 -0800

    heroku deploy

--- public/index.js
+++ public/index.js
@@ -1,8 +1,6 @@
 /* global axios */
 
-console.log("index.js connected");
-
 axios.get("http://localhost:3000/api/recipes").then(function(response){
 	var recipes = response.data;
 	console.log(recipes);
-})
+});
